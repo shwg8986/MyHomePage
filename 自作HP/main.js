4ee7246e2ff2dc540c4d9e8eb7701a6278235126
shwg8986 <67375194+shwg8986@users.noreply.github.com>
2022-04-26 14:08:40 +0900

    Add files via upload

---
+++ 自作HP/main.js
@@ -0,0 +1,28 @@
+'use strict'{
+
+  // $(function () {
+  //   // アコーディオン
+  //   $('.list-item').click(function () {
+  //     let $answer = $(this).find('.answer');
+  //     if ($answer.hasClass('open')) {
+  //       $answer.removeClass('open');
+  //       // slideUpメソッドを用いて、$answerを隠す
+  //       $answer.slideUp();
+
+  //       // 子要素のspanタグの中身をtextメソッドを用いて書き換え
+  //       $(this).find('span').text('+')
+
+  //     } else {
+  //       $answer.addClass('open');
+  //       // slideDownメソッドを用いて、$answerを表示
+  //       $answer.slideDown();
+
+  //       // 子要素のspanタグの中身をtextメソッドを用いて書き換え
+  //       $(this).find('span').text('-')
+
+  //     }
+  //   });
+
+  // });
+
+}
